03c2e47d526f9c02114d596dcf200972e871ba9c
Saud Badar <saud.badar@mial.utoronto.ca>
2022-11-15 06:03:13 -0500

    Project 3: Init Submission

--- Project 2 Parallax backgrounds with JavaScript/script.js
+++ Project 2 Parallax backgrounds with JavaScript/script.js
@@ -6,15 +6,15 @@ let gameSpeed = 5;
 //let gameFrame = 0;
 
 const backgroundLayer1 = new Image();
-backgroundLayer1.src = 'layer-1.png';
+backgroundLayer1.src = 'forest_layer-1.png';
 const backgroundLayer2 = new Image();
-backgroundLayer2.src = 'layer-2.png';
+backgroundLayer2.src = 'forest_layer-2.png';
 const backgroundLayer3 = new Image();
-backgroundLayer3.src = 'layer-3.png';
+backgroundLayer3.src = 'forest_layer-3.png';
 const backgroundLayer4 = new Image();
-backgroundLayer4.src = 'layer-4.png';
+backgroundLayer4.src = 'forest_layer-4.png';
 const backgroundLayer5 = new Image();
-backgroundLayer5.src = 'layer-5.png';
+backgroundLayer5.src = 'forest_layer-5.png';
 
 window.addEventListener('load', function(){
     const slider = document.getElementById('slider');
